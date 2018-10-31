e6ee683f189607a3f55a960bd4efead18a7199d9
unknown <yuanyuan.jing@office.freemud.cn>
2018-10-31 12:02:33 +0800

    Initial commit

---
+++ src/main/java/com/yy/mapper/UserInfoDao.java
@@ -0,0 +1,16 @@
+package com.yy.mapper;
+
+import com.yy.model.UserInfo;
+
+/**
+* 通用 Mapper 代码生成器
+*
+* @author mapper-generator
+*/
+public interface UserInfoDao extends tk.mybatis.mapper.common.Mapper<UserInfo> {
+
+}
+
+
+
+
---
+++ src/main/java/tk/mybatis/mapper/generator/MapperPlugin.java
@@ -0,0 +1,291 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2014-2017 abel533@gmail.com
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package tk.mybatis.mapper.generator;
+
+import org.mybatis.generator.api.IntrospectedColumn;
+import org.mybatis.generator.api.IntrospectedTable;
+import org.mybatis.generator.api.Plugin;
+import org.mybatis.generator.api.dom.java.FullyQualifiedJavaType;
+import org.mybatis.generator.api.dom.java.Interface;
+import org.mybatis.generator.api.dom.java.Method;
+import org.mybatis.generator.api.dom.java.TopLevelClass;
+import org.mybatis.generator.config.CommentGeneratorConfiguration;
+import org.mybatis.generator.config.Context;
+import org.mybatis.generator.internal.util.StringUtility;
+
+import java.util.HashSet;
+import java.util.Properties;
+import java.util.Set;
+
+/**
+ * 通用Mapper生成器插件
+ *
+ * @author liuzh
+ */
+public class MapperPlugin extends FalseMethodPlugin {
+    private Set<String> mappers                   = new HashSet<String>();
+    private boolean     caseSensitive             = false;
+    private boolean     useMapperCommentGenerator = true;
+    //开始的分隔符，例如mysql为`，sqlserver为[
+    private String      beginningDelimiter        = "";
+    //结束的分隔符，例如mysql为`，sqlserver为]
+    private String      endingDelimiter           = "";
+    //数据库模式
+    private String                        schema;
+    //注释生成器
+    private CommentGeneratorConfiguration commentCfg;
+    //强制生成注解
+    private boolean                       forceAnnotation;
+    
+    //是否需要生成Getter注解
+    private boolean needsGetter = false;
+    //是否需要生成Setter注解
+    private boolean needsSetter = false;
+    //是否需要生成ToString注解
+    private boolean needsToString = false;
+    //是否需要生成Accessors(chain = true)注解
+    private boolean needsAccessors = false;
+
+    public String getDelimiterName(String name) {
+        StringBuilder nameBuilder = new StringBuilder();
+        if (StringUtility.stringHasValue(schema)) {
+            nameBuilder.append(schema);
+            nameBuilder.append(".");
+        }
+        nameBuilder.append(beginningDelimiter);
+        nameBuilder.append(name);
+        nameBuilder.append(endingDelimiter);
+        return nameBuilder.toString();
+    }
+
+    /**
+     * 生成的Mapper接口
+     *
+     * @param interfaze
+     * @param topLevelClass
+     * @param introspectedTable
+     * @return
+     */
+    @Override
+    public boolean clientGenerated(Interface interfaze, TopLevelClass topLevelClass, IntrospectedTable introspectedTable) {
+        //获取实体类
+        FullyQualifiedJavaType entityType = new FullyQualifiedJavaType(introspectedTable.getBaseRecordType());
+        //import接口
+        for (String mapper : mappers) {
+            interfaze.addImportedType(new FullyQualifiedJavaType(mapper));
+            interfaze.addSuperInterface(new FullyQualifiedJavaType(mapper + "<" + entityType.getShortName() + ">"));
+        }
+        //import实体类
+        interfaze.addImportedType(entityType);
+        return true;
+    }
+
+    /**
+     * 处理实体类的包和@Table注解
+     *
+     * @param topLevelClass
+     * @param introspectedTable
+     */
+    private void processEntityClass(TopLevelClass topLevelClass, IntrospectedTable introspectedTable) {
+        //引入JPA注解
+        topLevelClass.addImportedType("javax.persistence.*");
+        //lombok扩展开始
+        //如果需要Getter，引入包，代码增加注解
+        if (this.needsGetter) {
+            topLevelClass.addImportedType("lombok.Getter");
+            topLevelClass.addAnnotation("@Getter");
+        }
+        //如果需要Setter，引入包，代码增加注解
+        if (this.needsSetter) {
+            topLevelClass.addImportedType("lombok.Setter");
+            topLevelClass.addAnnotation("@Setter");
+        }
+        //如果需要ToString，引入包，代码增加注解
+        if (this.needsToString) {
+            topLevelClass.addImportedType("lombok.ToString");
+            topLevelClass.addAnnotation("@ToString");
+        }
+        //如果需要Getter，引入包，代码增加注解
+        if (this.needsAccessors) {
+            topLevelClass.addImportedType("lombok.experimental.Accessors");
+            topLevelClass.addAnnotation("@Accessors(chain = true)");
+        }
+        //lombok扩展结束
+        String tableName = introspectedTable.getFullyQualifiedTableNameAtRuntime();
+        //如果包含空格，或者需要分隔符，需要完善
+        if (StringUtility.stringContainsSpace(tableName)) {
+            tableName = context.getBeginningDelimiter()
+                    + tableName
+                    + context.getEndingDelimiter();
+        }
+        //是否忽略大小写，对于区分大小写的数据库，会有用
+        if (caseSensitive && !topLevelClass.getType().getShortName().equals(tableName)) {
+            topLevelClass.addAnnotation("@Table(name = \"" + getDelimiterName(tableName) + "\")");
+        } else if (!topLevelClass.getType().getShortName().equalsIgnoreCase(tableName)) {
+            topLevelClass.addAnnotation("@Table(name = \"" + getDelimiterName(tableName) + "\")");
+        } else if (StringUtility.stringHasValue(schema)
+                || StringUtility.stringHasValue(beginningDelimiter)
+                || StringUtility.stringHasValue(endingDelimiter)) {
+            topLevelClass.addAnnotation("@Table(name = \"" + getDelimiterName(tableName) + "\")");
+        } else if (forceAnnotation) {
+            topLevelClass.addAnnotation("@Table(name = \"" + getDelimiterName(tableName) + "\")");
+        }
+    }
+    
+    /**
+     * 如果需要生成Getter注解，就不需要生成get相关代码了
+     */
+    @Override
+    public boolean modelGetterMethodGenerated(Method method,
+                                              TopLevelClass topLevelClass, IntrospectedColumn introspectedColumn,
+                                              IntrospectedTable introspectedTable,
+                                              Plugin.ModelClassType modelClassType) {
+
+        return !this.needsGetter;
+    }
+
+    /**
+     * 如果需要生成Setter注解，就不需要生成set相关代码了
+     */
+    @Override
+    public boolean modelSetterMethodGenerated(Method method,
+                                              TopLevelClass topLevelClass, IntrospectedColumn introspectedColumn,
+                                              IntrospectedTable introspectedTable,
+                                              Plugin.ModelClassType modelClassType) {
+        return !this.needsSetter;
+    }
+
+    /**
+     * 生成基础实体类
+     *
+     * @param topLevelClass
+     * @param introspectedTable
+     * @return
+     */
+    @Override
+    public boolean modelBaseRecordClassGenerated(TopLevelClass topLevelClass, IntrospectedTable introspectedTable) {
+        processEntityClass(topLevelClass, introspectedTable);
+        return true;
+    }
+
+    /**
+     * 生成实体类注解KEY对象
+     *
+     * @param topLevelClass
+     * @param introspectedTable
+     * @return
+     */
+    @Override
+    public boolean modelPrimaryKeyClassGenerated(TopLevelClass topLevelClass, IntrospectedTable introspectedTable) {
+        processEntityClass(topLevelClass, introspectedTable);
+        return true;
+    }
+
+    /**
+     * 生成带BLOB字段的对象
+     *
+     * @param topLevelClass
+     * @param introspectedTable
+     * @return
+     */
+    @Override
+    public boolean modelRecordWithBLOBsClassGenerated(TopLevelClass topLevelClass, IntrospectedTable introspectedTable) {
+        processEntityClass(topLevelClass, introspectedTable);
+        return false;
+    }
+
+
+    @Override
+    public void setContext(Context context) {
+        super.setContext(context);
+        //设置默认的注释生成器
+        useMapperCommentGenerator = !"FALSE".equalsIgnoreCase(context.getProperty("useMapperCommentGenerator"));
+        if (useMapperCommentGenerator) {
+            commentCfg = new CommentGeneratorConfiguration();
+            commentCfg.setConfigurationType(MapperCommentGenerator.class.getCanonicalName());
+            context.setCommentGeneratorConfiguration(commentCfg);
+        }
+        //支持oracle获取注释#114
+        context.getJdbcConnectionConfiguration().addProperty("remarksReporting", "true");
+    }
+
+    @Override
+    public void setProperties(Properties properties) {
+        super.setProperties(properties);
+        String mappers = this.properties.getProperty("mappers");
+        if (StringUtility.stringHasValue(mappers)) {
+            for (String mapper : mappers.split(",")) {
+                this.mappers.add(mapper);
+            }
+        } else {
+            throw new RuntimeException("Mapper插件缺少必要的mappers属性!");
+        }
+        String caseSensitive = this.properties.getProperty("caseSensitive");
+        if (StringUtility.stringHasValue(caseSensitive)) {
+            this.caseSensitive = "TRUE".equalsIgnoreCase(caseSensitive);
+        }
+        String forceAnnotation = this.properties.getProperty("forceAnnotation");
+        if (StringUtility.stringHasValue(forceAnnotation)) {
+            if (useMapperCommentGenerator) {
+                commentCfg.addProperty("forceAnnotation", forceAnnotation);
+            }
+            this.forceAnnotation = "TRUE".equalsIgnoreCase(forceAnnotation);
+        }
+        String beginningDelimiter = this.properties.getProperty("beginningDelimiter");
+        if (StringUtility.stringHasValue(beginningDelimiter)) {
+            this.beginningDelimiter = beginningDelimiter;
+        }
+        String endingDelimiter = this.properties.getProperty("endingDelimiter");
+        if (StringUtility.stringHasValue(endingDelimiter)) {
+            this.endingDelimiter = endingDelimiter;
+        }
+        String schema = this.properties.getProperty("schema");
+        if (StringUtility.stringHasValue(schema)) {
+            this.schema = schema;
+        }
+        
+        //lombok扩展
+        String lombok = this.properties.getProperty("lombok");
+        if (lombok != null && !"".equals(lombok)) {
+            if (lombok.contains("Getter")) {
+                this.needsGetter = true;
+            }
+            if (lombok.contains("Setter")) {
+                this.needsSetter = true;
+            }
+            if (lombok.contains("ToString")) {
+                this.needsToString = true;
+            }
+            if (lombok.contains("Accessors")) {
+                this.needsAccessors = true;
+            }
+        }
+        
+        if (useMapperCommentGenerator) {
+            commentCfg.addProperty("beginningDelimiter", this.beginningDelimiter);
+            commentCfg.addProperty("endingDelimiter", this.endingDelimiter);
+        }
+    }
+}
